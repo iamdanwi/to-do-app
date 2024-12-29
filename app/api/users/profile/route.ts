3ad147a3097331b533b800e0675ce106a7f438d5
iamdanwi <danwichoudhary@gmail.com>
2024-12-29 16:13:01 +0530

    Enhance user profile retrieval API to handle errors and return appropriate responses

--- app/api/users/profile/route.ts
+++ app/api/users/profile/route.ts
@@ -5,13 +5,18 @@ import User from "@/models/user.model";
 
 connectDB();
 
-export default async function GET(req: NextRequest) {
-    const userId = await getDataFromToken(req);
-    const user = User.findOne({ _id: userId }).select("-password");
+export async function GET(req: NextRequest) {
+    try {
+        const userId = await getDataFromToken(req);
+        const user = await User.findOne({ _id: userId }).select("-password");
 
-    if (!user) {
-        return NextResponse.error();
+        if (!user) {
+            return NextResponse.json({ error: "User not found" }, { status: 404 });
+        }
+
+        return NextResponse.json(user);
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (error) {
+        return NextResponse.json({ error: "An error occurred" }, { status: 500 });
     }
-
-    return NextResponse.json(user);
 }
